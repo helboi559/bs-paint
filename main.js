232a8005e8d477056e99963b4a8e7c4ac6148512
Juan Ramirez <rami8224.2007@gmail.com>
2022-03-23 16:11:25 -0400

    changed color of assignment

--- main.js
+++ main.js
@@ -17,27 +17,14 @@
  */
 const gridWidth = 10;
 let count = 0;
+const canvas = document.querySelector('.canvas');
 while (count <= gridWidth * gridWidth) {
-  const canvas = document.querySelector('.canvas');
   const div = document.createElement('div');
   div.className = 'square color-5';
   canvas.appendChild(div);
   count++;
 }
 
-// You probably should NOT do these in the order below.
-// That is, you probably should NOT do all the queries,
-// THEN all the functions,
-// THEN all the wiring.
-
-// Instead, it'll be easier if you go one action at a time!
-// So, add a query for the palette colors.
-// THEN add an event listener function for what happens when one is clicked.
-// THEN wire those two together, so that when the palette elements are clicked,
-// the function runs.
-//
-// And proceed from there to getting the squares working.
-//
 
 // ALSO.
 // You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
@@ -48,13 +35,47 @@ while (count <= gridWidth * gridWidth) {
 
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
-
-
-
+// let app = document.querySelector('.app')
+let paletteColors = document.querySelectorAll('.brush-selection div')
+// console.log(paletteColors)
+// Target Canvas list by "grid"
+let canvasGrid = document.querySelectorAll('.canvas div')
+// console.log(canvasGrid)
+//Current brush
+let currentBrush = document.querySelector('.current-brush')
+let brushList = currentBrush.classList
+// console.log(currentBrush.classList)
 /****************************
  * EVENT LISTENER FUNCTIONS *
 ****************************/
-
+//any color clicked- changes the color of brush
+for (let color of paletteColors) {
+  // console.log(color.classList[1])
+  color.addEventListener("click", ()=> {
+    //change colors
+    brushList.replace(brushList[1],color.classList[1])
+    // console.log(brushList[1])
+  })
+}
+let mouseUp = false;
+for (let box of canvasGrid) {
+  box.addEventListener("click", ()=> {
+    box.classList.replace(box.classList[1],brushList[1])
+  })
+  box.addEventListener("mouseenter", ()=> {
+    box.classList.replace(box.classList[1],brushList[1])
+  })
+}
+//ONE COLOR-------TESTING
+// let oneColor = document.querySelector('.color-1');
+// let oneColorList = oneColor.classList;
+// console.log(oneColor.classList)
+// oneColor.addEventListener('click',()=> {
+ 
+//   brushList.replace(brushList[1],oneColorList[1])
+  
+// })
+//---------
 // Now add some functions to handle clicking one particular square
 // and clicking one particular palette color. You can leave them
 // empty at first, though a console.log just to know they're being
