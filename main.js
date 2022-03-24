118cf567231adf72732ba2dd58860d42e65289da
Juan Ramirez <rami8224.2007@gmail.com>
2022-03-24 15:55:19 -0400

    added colors to drag

--- main.js
+++ main.js
@@ -1,43 +1,27 @@
-/*******************
- * OUR HELPER CODE *
-*******************/
-
-/*
- * Here we add the squares to the canvas dynamically.
- * You can mostly leave this section alone!
- * But if you want to change how wide the canvas is,
- * there are just two steps:
- * 
- * 1. Change the `gridWidth` value below.
- * 2. Change the `grid-template-rows` and
- * `grid-template-columns` to match.
- *
- * To make the second one happen, the number to change
- * is the first argument to `repeat`, currently set at 10.
- */
-const gridWidth = 30;
+//HELPER CODE----
+const gridWidth = 50;
 let count = 0;
 const canvas = document.querySelector('.canvas');
 while (count <= gridWidth * gridWidth) {
   const div = document.createElement('div');
-  div.className = 'square color-5';
+  div.className = 'square color-7';
   canvas.appendChild(div);
   count++;
 }
-
-
-// ALSO.
-// You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
 
 /***********
  * QUERIES *
 ***********/
 //adding colors-----------------------
+let drag = false //--global var
 let palette = document.querySelector('.palette')
 let newColor = document.createElement('div')
 newColor.className = "palette-color color-6"
+let baseColor = document.createElement('div')
+baseColor.className = "palette-color color-7"
 // console.log(newColor)
 palette.appendChild(newColor)
+palette.appendChild(baseColor)
 //---------------------------------
 // IDENTIFY ALL COLORS ETC --------------------
 let paletteColors = document.querySelectorAll('.brush-selection div')
@@ -50,48 +34,46 @@ let canvasGrid = document.querySelectorAll('.canvas div')
 let currentBrush = document.querySelector('.current-brush')
 let brushList = currentBrush.classList
 // console.log(brushList)
-const app = document.querySelector('.app')
+let app = document.querySelector('.app')
 /****************************
  * EVENT LISTENER FUNCTIONS *
 ****************************/
 //any color clicked- changes the color of brush
 // console.log(paletteColors)
 for (let color of paletteColors) {
-  console.log(color.classList)
+  // console.log(color.classList)
   color.addEventListener("click", ()=> {
     //change colors
     brushList.replace(brushList[1],color.classList[1])
     // console.log(brushList[1])
   })
+  //MIXING COLORS
+  // color.addEventListener('dblclick', () => {
+  //   brushList.replace(brushList[1],)
+  // })
 }
 //paint colors canvas
-// let drag = false;
 
-// app.addEventListener('mousedown', () => drag = false);
-// app.addEventListener('mousemove', () => drag = true);
-// app.addEventListener('mouseup', () => console.log(drag ? 'drag' : 'click'));
-let drag = false
+//WHAT TO COLOR------------
 for (let box of canvasGrid) {
-  
-  box.addEventListener("mousedown", ()=> {
+  box.addEventListener('mouseup',()=> {
+    drag=false
+    console.log(drag)
+})
+box.addEventListener('mousedown',()=> {
     box.classList.replace(box.classList[1],brushList[1])
-    // drag = true;
-    box.addEventListener('mousemove', ()=> {
+    drag=true
+    console.log(drag)
+})
+box.addEventListener('mouseenter',()=> {
+    if(drag == true) {
       box.classList.replace(box.classList[1],brushList[1])
-    })
-  })
-  
-  box.addEventListener("mouseup", ()=> {
-    
-    box.removeEventListener('mousemove', ()=> {
-      box.classList.replace(box.classList[1],brushList[1])
-      // drag = false
-    })
-    
-  })
+    }
+})
 }
+
 // console.log(brushList[1])
-//add dark theme toggle
+//add dark theme toggle------------------
 let body = document.querySelector('body')
 let darkTheme = document.createElement('button')
 darkTheme.innerText = "Dark Theme!"
@@ -100,3 +82,5 @@ app.appendChild(darkTheme)
 darkTheme.addEventListener('click', ()=> {
   body.style.backgroundColor = "gray"
 })
+
+
